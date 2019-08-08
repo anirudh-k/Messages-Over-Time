375e99ee3effd1bda319f51f6d2eaaf98ab3c91f
Anirudh Kaushik <anirudh.kaushik0@gmail.com>
2019-08-08 19:11:33 -0400

    whatsapp reader counts messages now

--- whatsapp_reader.py
+++ whatsapp_reader.py
@@ -4,6 +4,9 @@ import csv
 import re
 import shutil
 
+
+MESSAGES_DIR = 'data/whatsapp/'
+
 # message format:
 # [%m/%d/%y, %I:%M:%S %p] <Sender>: <message>
 # e.g.
@@ -13,10 +16,58 @@ class WhatsAppReader:
         self.fileName = fileName
         self.user = fileName.split('/')[-1][:fileName.split('/')[-1].index('_')]
 
+    def addToWindows(self, windowFile):
+        tempfile = NamedTemporaryFile(delete=False)
+        lineCount = 0
+        with open(MESSAGES_DIR + self.user + '_whatsapp.txt') as messages, open(windowFile) as windows, tempfile:
+            for line in messages:
+                lineCount += 1
+            messages.seek(0)
+
+            messageCount = 0
+
+            reader = csv.reader(windows, delimiter=',', quotechar='\'')
+            writer = csv.writer(tempfile, delimiter=',', quotechar='\'')
+
+            firstRow = True
+
+            curMessage = messages.readline()
+            messageCount = 0 # first message doesn't count
+            for row in reader:
+                # skip first window row because it's the header
+                if firstRow == True:
+                    idx = row.index(self.user)
+                    writer.writerow(row)
+                    firstRow = False
+                    continue
+                windowDT = self.timeOfWindow(row[0])
+                curMessageDT = self.timeOfMessage(curMessage)
+                while curMessageDT <= windowDT:
+                    curMessage = messages.readline()
+                    if curMessage[0:3] == '\xe2\x80\x8e': # some (valid) messages have this in front idk why
+                        curMessage = curMessage[3:]
+                    if curMessage == '': # readline() returns empty string when it's reached EOF
+                        break
+                    while not (curMessage[0] == '[' and ('AM]' in curMessage or 'PM]' in curMessage) and ('] Annie Wu:' in curMessage or '] Anirudh Kaushik:' in curMessage)):
+                        curMessage = messages.readline()
+                        if curMessage[0:3] == '\xe2\x80\x8e': # some (valid) messages have this in front idk why
+                            curMessage = curMessage[3:]
+                        if curMessage == '': # readline() returns empty string when it's reached EOF
+                            break
+                    curMessageDT = self.timeOfMessage(curMessage)
+                    messageCount += 1
+
+                row[idx] = messageCount - 1
+                writer.writerow(row)
+        shutil.copy(tempfile.name, windowFile)
+
     def timeOfMessage(self, message):
-        dtString = re.search('\[(.*)\]', message).group(1)
+        dtString = re.search('\[(.*)M\]', message).group(1) + 'M'
         dt = datetime.strptime(dtString, '%m/%d/%y, %I:%M:%S %p')
         return dt
+
+    def timeOfWindow(self, window):
+        return datetime.strptime(window, '%Y-%m-%d %H:%M:%S')
 
     def oldestMessage(self):
         with open(self.fileName) as f:
@@ -28,24 +79,3 @@ class WhatsAppReader:
             lines = f.readlines()
             line = lines[len(lines) - 2] # the true last line is an empty line
             return self.timeOfMessage(line)
-
-    def addToWindows(self, windowFile):
-        tempfile = NamedTemporaryFile(delete=False)
-        with open(windowFile) as f, tempfile:
-            reader = csv.reader(f, delimiter=',', quotechar='\'')
-            writer = csv.writer(tempfile, delimiter=',', quotechar='\'')
-
-            messageCount = 8
-            firstRow = True
-            # idx = -1
-            for row in reader:
-                if firstRow == True:
-                    idx = row.index(self.user)
-                    writer.writerow(row)
-                    firstRow = False
-                    continue
-                # print(row)
-                row[idx] = messageCount
-                writer.writerow(row)
-                messageCount += 1
-        shutil.copy(tempfile.name, windowFile)
