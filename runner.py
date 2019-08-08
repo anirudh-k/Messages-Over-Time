228a8077444eaef9260231da3e3d66fcd5336ac4
Anirudh Kaushik <anirudh.kaushik0@gmail.com>
2019-08-08 13:22:45 -0400

    have runner create empty windows

---
+++ runner.py
@@ -0,0 +1,10 @@
+from reader import Reader
+
+r = Reader()
+r.initWindows()
+
+# create
+# whatsAppReader = WhatsAppReader('data/whatsapp/annie_whatsapp.txt')
+# print(whatsAppReader.oldestMessage())
+# print(whatsAppReader.newestMessage())
+# print(whatsAppReader.addToWindows('blah'))
