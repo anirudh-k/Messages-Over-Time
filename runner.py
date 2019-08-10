1ca230e3295707d7118747a250f8555e5ddafc43
Anirudh Kaushik <anirudh.kaushik0@gmail.com>
2019-08-10 00:43:34 -0400

    keep runner simple

--- runner.py
+++ runner.py
@@ -3,9 +3,3 @@ from reader import Reader
 r = Reader()
 r.initWindows()
 r.addToWindows()
-
-# create
-# whatsAppReader = WhatsAppReader('data/whatsapp/annie_whatsapp.txt')
-# print(whatsAppReader.oldestMessage())
-# print(whatsAppReader.newestMessage())
-# print(whatsAppReader.addToWindows('blah'))
